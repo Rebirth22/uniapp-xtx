312961d418fa4923de0096322538991943eaeac6
Rebirth22 <liuguoquan6707@gmail.com>
2023-08-24 01:29:46 +0800

    feat: 动态获取热门推荐数据+页面跳转页面渲染+导航栏和tab渲染+商品渲染

---
+++ src/pages/hot/hot.vue
@@ -0,0 +1,203 @@
+<!-- 热门推荐的详细路由 -->
+<script setup lang="ts">
+import { getHotRecommendAPI } from '@/services/hot'
+import type { SubTypeItem } from '@/types/hot'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+
+// 热门推荐页 标题和url
+const urlMap = [
+  { type: '1', title: '特惠推荐', url: '/hot/preference' },
+  { type: '2', title: '爆款推荐', url: '/hot/inVogue' },
+  { type: '3', title: '一站买全', url: '/hot/oneStop' },
+  { type: '4', title: '新鲜好物', url: '/hot/new' },
+]
+
+// uniapp 获取页面参数----首页的HotPanel中包含跳转链接，获取其中的参数，参数是接口获取到的数据
+const query = defineProps<{
+  type: string
+}>()
+
+// 匹配设置好的标题
+const currUrlMap = urlMap.find((v) => v.type === query.type)
+// 动态设置导航栏标题
+uni.setNavigationBarTitle({ title: currUrlMap!.title })
+
+// 推荐商品封面图
+const bannerPicture = ref('')
+// 推荐选项----tab导航选项
+const subTypes = ref<SubTypeItem[]>([])
+// tab栏高亮的下标
+const activeIndex = ref(0)
+
+// 获取热门推荐数据
+const getHotRecommendData = async () => {
+  const res = await getHotRecommendAPI(currUrlMap!.url)
+  console.log(res)
+
+  bannerPicture.value = res.result.bannerPicture //商品封面图片
+  subTypes.value = res.result.subTypes //tab导航选项
+}
+
+// 页面加载
+onLoad(() => {
+  getHotRecommendData()
+})
+</script>
+
+<template>
+  <view class="viewport">
+    <!-- 推荐封面图 -->
+    <view class="cover">
+      <image :src="bannerPicture"> </image>
+    </view>
+    <!-- 推荐选项 -->
+    <view class="tabs">
+      <!--:class="{ active: index === activeIndex }"：绑定tab栏高亮的部分
+      @tap="activeIndex = index"：tab栏选中的对应高亮-->
+      <text
+        v-for="(item, index) in subTypes"
+        :key="item.id"
+        class="text"
+        :class="{ active: index === activeIndex }"
+        @tap="activeIndex = index"
+        >{{ item.title }}</text
+      >
+    </view>
+    <!-- tab栏对应的推荐列表 -->
+    <!-- v-show="activeIndex === index":选中才显示，v-if会销毁 -->
+    <scroll-view
+      v-for="(item, index) in subTypes"
+      :key="item.id"
+      v-show="activeIndex === index"
+      scroll-y
+      class="scroll-view"
+    >
+      <view class="goods">
+        <navigator
+          hover-class="none"
+          class="navigator"
+          v-for="goods in item.goodsItems.items"
+          :key="goods.id"
+          :url="`/pages/goods/goods?id=${goods.id}`"
+        >
+          <image class="thumb" :src="goods.picture"></image>
+          <view class="name ellipsis">{{ goods.name }}</view>
+          <view class="price">
+            <text class="symbol">¥</text>
+            <text class="number">{{ goods.price }}</text>
+          </view>
+        </navigator>
+      </view>
+      <view class="loading-text">正在加载...</view>
+    </scroll-view>
+  </view>
+</template>
+
+<style lang="scss">
+page {
+  height: 100%;
+  background-color: #f4f4f4;
+}
+
+.viewport {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 180rpx 0 0;
+  position: relative;
+}
+
+.cover {
+  width: 750rpx;
+  height: 225rpx;
+  border-radius: 0 0 40rpx 40rpx;
+  overflow: hidden;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+
+.scroll-view {
+  flex: 1;
+}
+
+.tabs {
+  display: flex;
+  justify-content: space-evenly;
+  height: 100rpx;
+  line-height: 90rpx;
+  margin: 0 20rpx;
+  font-size: 28rpx;
+  border-radius: 10rpx;
+  box-shadow: 0 4rpx 5rpx rgba(200, 200, 200, 0.3);
+  color: #333;
+  background-color: #fff;
+  position: relative;
+  z-index: 9;
+
+  .text {
+    margin: 0 20rpx;
+    position: relative;
+  }
+
+  .active {
+    &::after {
+      content: '';
+      width: 40rpx;
+      height: 4rpx;
+      transform: translate(-50%);
+      background-color: #27ba9b;
+      position: absolute;
+      left: 50%;
+      bottom: 24rpx;
+    }
+  }
+}
+
+.goods {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 0 20rpx 20rpx;
+
+  .navigator {
+    width: 345rpx;
+    padding: 20rpx;
+    margin-top: 20rpx;
+    border-radius: 10rpx;
+    background-color: #fff;
+  }
+
+  .thumb {
+    width: 305rpx;
+    height: 305rpx;
+  }
+
+  .name {
+    height: 88rpx;
+    font-size: 26rpx;
+  }
+
+  .price {
+    line-height: 1;
+    color: #cf4444;
+    font-size: 30rpx;
+  }
+
+  .symbol {
+    font-size: 70%;
+  }
+
+  .decimal {
+    font-size: 70%;
+  }
+}
+
+.loading-text {
+  text-align: center;
+  font-size: 28rpx;
+  color: #666;
+  padding: 20rpx 0 50rpx;
+}
+</style>
