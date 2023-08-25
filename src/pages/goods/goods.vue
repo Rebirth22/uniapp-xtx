c81e69333d2c647eeb25d62954e8e0a420045f52
Rebirth22 <liuguoquan6707@gmail.com>
2023-08-26 06:04:09 +0800

    feat: 购物车模块：带返回按钮的购物车（不在tabBar注册，复用页面）

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -252,7 +252,8 @@ const onAddCart = async (ev: SkuPopupEvent) => {
       <button class="icons-button" open-type="contact">
         <text class="icon-handset"></text>客服
       </button>
-      <navigator class="icons-button" url="/pages/cart/cart" open-type="switchTab">
+      <!-- url="/pages/cart/cart2" :从商品详情去购物车的跳转在tabbar顶部会出现回转箭头 -->
+      <navigator class="icons-button" url="/pages/cart/cart2" open-type="navigate">
         <text class="icon-cart"></text>购物车
       </navigator>
     </view>
